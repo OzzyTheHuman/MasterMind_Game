af6a3d2c143268703c84ac1db697085bdf5626b8
OzzyTheHuman <151674182+OzzyTheHuman@users.noreply.github.com>
2025-12-20 17:10:48 +0100

    feat: Create basic properties

---
+++ MasterMind_Game/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace MasterMind_Game;
+
+public class Game
+{
+    public int Rounds { get; private set; }
+    public int CodeLength { get; private set; }
+    public int ColorsCount { get; private set; }
+
+    private List<int> _secretCode;
+    private static readonly List<string> _allColors = ["red", "yellow", "green", "blue", "magenta", "cyan"];
+
+    public Game(int rounds = 9, int codeLength = 4, int colorsCount = 6)
+    {
+        Rounds = rounds;
+        CodeLength = codeLength;
+        ColorsCount = colorsCount;
+    }
+}
