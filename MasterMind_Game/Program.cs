08a5747fa3d49256c47c5d739e2ed6dae6535682
OzzyTheHuman <151674182+OzzyTheHuman@users.noreply.github.com>
2025-12-21 12:12:53 +0100

    feat: Implement simple ui

--- MasterMind_Game/Program.cs
+++ MasterMind_Game/Program.cs
@@ -1,9 +1,69 @@
-﻿namespace MasterMind_Game;
+﻿using System.Reflection.Metadata.Ecma335;
 
+namespace MasterMind_Game;
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool gameRunning = true;
+        
+        Console.Clear();
+        Console.WriteLine("                                                                                                                    ");
+        Console.Clear();
+        
+        while (gameRunning)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Mastermind The Game ===");
+            Console.WriteLine("1. New Game");
+            Console.WriteLine("2. Quit");
+            
+            string input = Console.ReadLine();
+            Console.Clear();
+            
+            switch (input)
+            {
+                case "1":
+                    PlayGame();
+                    break;
+                case "2":
+                    gameRunning = false;
+                    break;
+            }
+        }
+
+        Console.Clear();
+        Console.WriteLine("Thanks for playing !");
+    }
+
+    static void PlayGame()
+    {
+        var game = new Game();
+        while (true)
+        {
+            Console.WriteLine(ParseSecretCodeToString(game.GetSecretCode()));
+            string input = Console.ReadLine();
+            // TODO: FIX System.ArgumentOutOfRangeException WHEN PLAYER ENTERS INPUT THAT IS TOO LONG
+            if (input.ToLower() == "q" || input.ToLower() == "quit")
+            {
+                Console.WriteLine($"Correct answer was : {ParseSecretCodeToString(game.GetSecretCode())}");
+                Console.WriteLine("Press any button to continue ...");
+                Console.ReadKey();
+                break;
+            }
+            Console.WriteLine(game.CheckForAnswers(input));
+        }
+    }
+
+    static string ParseSecretCodeToString(List<string> list)
+    {
+        string result = "";
+        foreach (var n in list)
+        {
+            result += "[" + n + "]";
+        }
+
+        return result;
     }
 }
