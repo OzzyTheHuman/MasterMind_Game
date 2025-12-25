e0efd8862b23f5656c3ed74ad1edd1aa0ad39b7f
OzzyTheHuman <151674182+OzzyTheHuman@users.noreply.github.com>
2025-12-26 00:25:06 +0100

    feat: Implement custom game settings

--- MasterMind_Game/Program.cs
+++ MasterMind_Game/Program.cs
@@ -23,8 +23,89 @@ class Program
             switch (input)
             {
                 case "1":
-                    Game.DeleteSavedGame();
-                    PlayGame(new Game(colorsCount:6));
+                    Console.WriteLine("=== Select variant ===\n");
+                    Console.WriteLine("1. Normal");
+                    Console.WriteLine("   - colors: 6");
+                    Console.WriteLine("   - code length: 4");
+                    Console.WriteLine("   - rounds: 9\n");
+                    Console.WriteLine("2. Custom");
+                    Console.WriteLine("   - Configure all options to your liking\n");
+                    Console.WriteLine("3. Go back");
+                    
+                    string input2 = Console.ReadLine();
+                    ClearConsole();
+                    
+                    Console.WriteLine("=== Custom ===\n");
+                    bool choosingGameVariant = true;
+                    while (choosingGameVariant)
+                    {
+                        Game.DeleteSavedGame();
+                        switch (input2)
+                        {
+                            case "1":
+                                PlayGame(new Game(colorsCount: 6));
+                                choosingGameVariant = false;
+                                break;
+                            
+                            case "2":
+                                bool configuringGame = true;
+                                int colorsCount = 0;
+                                int codeLength = 0;
+                                int roundsCount = 0;
+                                while (configuringGame)
+                                {
+                                    Console.Write("Colors (default is 6, pick between 6 and 8): ");
+                                    string inputColors = Console.ReadLine();
+                                    if (int.TryParse(inputColors, out colorsCount))
+                                    {
+                                        if (colorsCount is >= 6 and <= 8)
+                                        {
+                                            break;
+                                        }
+                                    }
+
+                                    ClearConsoleOneLine();
+                                }
+                                while (configuringGame)
+                                {
+                                    Console.Write($"Code length (default is 4, pick between 4 and {colorsCount}): ");
+                                    string inputCodeLength = Console.ReadLine();
+                                    if (int.TryParse(inputCodeLength, out codeLength))
+                                    {
+                                        if (codeLength >= 4 && codeLength <= colorsCount)
+                                        {
+                                            break;
+                                        }
+                                    }
+
+                                    ClearConsoleOneLine();
+                                }
+                                while (configuringGame)
+                                {
+                                    Console.Write("Number of rounds (default is 9, pick between 9 and 11): ");
+                                    string roundsInput = Console.ReadLine();
+                                    if (int.TryParse(roundsInput, out roundsCount))
+                                    {
+                                        if (roundsCount >= 9 && roundsCount <= 11)
+                                        {
+                                            break;
+                                        }
+                                    }
+
+                                    ClearConsoleOneLine();
+                                }
+
+                                ClearConsole();
+                                PlayGame(new Game(roundsCount, codeLength, colorsCount));
+                                choosingGameVariant = false;
+                                break;
+                            
+                            case "3":
+                                choosingGameVariant = false;
+                                break;
+                        }
+                    }
+
                     break;
                 case "2":
                     if (Game.HasSavedGame())
@@ -65,8 +146,11 @@ class Program
             if (guess.ToLower().Trim() == "q" || guess.ToLower().Trim() == "quit")
             {
                 game.Surrender();
-                Console.WriteLine();
-                Console.WriteLine("Game progress is saved, you can close the game");
+                if (game.CurrentRound != 1)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Game progress is saved, you can close the game");
+                }
                 WaitForResponse();
                 continue;
             }
@@ -112,6 +196,13 @@ class Program
         Console.WriteLine("\x1b[2J");
         Console.WriteLine("\x1b[3J");
         Console.Clear();
+    }
+
+    static void ClearConsoleOneLine()
+    {
+        Console.CursorTop--;
+        Console.WriteLine("\x1b[2K");
+        Console.CursorTop--;
     }
     static void ShowSavedGame(Game game)
     {
